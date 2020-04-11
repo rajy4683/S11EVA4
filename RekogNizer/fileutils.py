3896acefb63042bd1991f5c1c016dc73a927bd69
rajy4683 <st.hazard@gmail.com>
2020-04-11 22:12:02 +0530

    Update fileutils.py

--- RekogNizer/fileutils.py
+++ RekogNizer/fileutils.py
@@ -14,6 +14,45 @@ from RekogNizer.hyperparams import *
 #from RekogNizer import 
 
 
+"""
+    shark_teeth_generator: generates a list of cyclic values within a given range
+    range_start: start value of the range 
+    range_end: ending value of the range
+    steps: steps over which the cycle needs to be calculated.  
+    returns: list of cyclic values within the given range
+"""
+def shark_teeth_generator(range_start,range_end,steps, ):
+    mult_factor = (range_end - range_start)/(steps)    
+    mid_point = np.floor(steps/2)
+    out_arr = []
+    for val in range(steps):
+        if val <= mid_point:
+            out_arr.append(range_start+(mult_factor*val))
+        else:
+            out_arr.append(range_start+(mult_factor*(2*mid_point-val)))
+    return out_arr
+"""
+    saw_graph_generator: Plots a saw/saw-like graph for a given number of iterations and frequency
+    total_steps: Total number of steps to plot the graph for 
+    single_step_len: Total number of steps per cycle
+    y_start: starting range of each cycle  
+    y_end: ending range of each cycle  
+    returns: list of cyclic values within the given range
+"""
+def saw_graph_generator(total_steps, single_step_len, y_start, y_end):
+    fig = plt.figure(figsize=(20,5))
+    start_range = 1
+    total_elements = total_steps
+    large_array = np.arange(1, total_elements+1, 1)
+    slice_length = single_step_len
+    output_array=[]
+    for i in range(int(np.floor(total_elements/slice_length))):
+        slice_local =large_array[slice_length*i:slice_length*(i+1)]
+        out_arr = shark_teeth_generator(y_start,y_end,slice_length)
+        output_array.append(out_arr)
+    plt.plot(large_array[:int(slice_length * np.floor(total_elements/slice_length))],np.reshape(output_array, -1))
+
+
 def show_sample_images(images, labels, classes, max_count=25):
     print(images.shape, torch.mean(images,[0,2,3]),torch.std(images,[0,2,3]))
     fig = plt.figure(figsize=(10,10))
